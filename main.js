20a6031ca44397e3a528a248f0864bb6907d1ab0
Maria Clara Silva <maria.silvamelo@eaportal.org>
2025-06-01 10:35:12 -0300

    adição do controle de tempo para que suma a divWarning depois de 4 segundos

--- main.js
+++ main.js
@@ -44,6 +44,10 @@ function removeItem(itemName) {
     const divWarning = document.querySelector(".warning")
 
     divWarning.classList.remove("hide-warning")
+
+    setTimeout(() => {
+        divWarning.classList.add("hide-warning")
+    }, 4000)
    
     if (itemIndex !== -1) {
         items.splice(itemIndex, 1)
